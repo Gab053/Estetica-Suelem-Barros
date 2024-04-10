356e7ddb49541d8e3c0b8079b8ffc93be5003a0a
gabrieldejesus <gabriel053jesus@gmail.com>
2024-04-10 18:09:31 -0300

    Update

--- site.py
+++ site.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 # Colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
