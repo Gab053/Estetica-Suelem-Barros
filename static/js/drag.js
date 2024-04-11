be0feb7cd7a5186410df222e650b83917fbac1df
gabrieldejesus <gabriel053jesus@gmail.com>
2024-04-11 16:28:13 -0300

    Update home

--- static/js/drag.js
+++ static/js/drag.js
@@ -1,6 +1,28 @@
 const carrossel = document.querySelector(".carrossel");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCardWidth = carrossel.querySelector(".card").offsetWidth;
+const carrosselChildrens = [...carrossel.children];
 
 let isDragging = false, startX, startScrollLeft;
+
+let cardPerView = Math.round(carrossel.offsetWidth / firstCardWidth);
+
+//copias de cards finais para carrossel infinito
+carrosselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carrossel.insertAdjacentHTML("afterbegin", card.outerHTML)
+});
+//copias de cards iniciais para carrossel infinito
+carrosselChildrens.slice(0, cardPerView).forEach(card => {
+    carrossel.insertAdjacentHTML("beforeend", card.outerHTML)
+});
+
+
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carrossel.scrollLeft += btn.id === "left" ? -firstCardWidth :  firstCardWidth;
+    })
+});
 
 const dragStart = (e) => {
     isDragging = true;
@@ -22,6 +44,26 @@ const dragStop = () => {
     carrossel.classList.remove("dragging");
 }
 
+const infiniteScroll = () => {
+    if(carrossel.scrollLeft === 0){
+        console.log("aiaiuiui");
+    } else if(carrossel.scrollLeft === carrossel.scrollWidth - carrossel.offsetWidth){
+        console.log("aaaaaaaa");
+    }
+}
+
 carrossel.addEventListener("mousedown", dragStart);
 carrossel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
+carrossel.addEventListener("scroll", infiniteScroll);
+
+
+
+
+document.getElementById("irSobre").addEventListener("click", function() {
+    window.scrollTo({
+        top: 1140,
+        behavior: "smooth"
+    });
+});
+
