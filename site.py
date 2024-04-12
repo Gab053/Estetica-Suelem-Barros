ddba0f4e765e1ed2d91edd1f68dc3bf6e7495c33
gabrieldejesus <gabriel053jesus@gmail.com>
2024-04-12 08:25:20 -0300

    Deploy test

--- site.py
+++ site.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 # Página Inicial
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
 
 # Colocar o site no ar
 if __name__ == "__main__":
